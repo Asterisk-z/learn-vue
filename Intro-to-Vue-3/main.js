3ad07bd08d861eac2a151558e49e54e9ac5b9949
Olang Daniel <olangdan17@gmail.com>
2023-11-11 06:44:39 +0100

    added counter

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -1,1 +1,18 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'the socks is white',
+            counter: 0,
+        }
+    },
+    methods: {
+        increase() {
+            this.counter++
+        },
+        decrease() {
+            this.counter--
+        }
+    },
+});
+ const mountApp = app.mount('#app')
